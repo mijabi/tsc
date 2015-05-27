95ce514ea9ec1e60e8cbb803d2ae77e6c9a40c7e
mijabi <mijabi@gmail.com>
2015-05-27 12:25:25 +0900

    引数の値で constructor で初期化するけど、引数がない場合も考慮する場合

--- sec6.js
+++ sec6.js
@@ -1,8 +1,14 @@
-//引数の値で constructor で初期化する場合
+// 引数の値で constructor で初期化するけど、引数がない場合も考慮する場合
 var A = (function () {
     function A(m) {
-        this.message = m;
+        if (m == undefined)
+            this.message = 'no value';
+        else
+            this.message = m;
     }
     return A;
 })();
 var a = new A('Hi');
+alert(a.message);
+var b = new A();
+alert(b.message);
--- sec6.ts
+++ sec6.ts
@@ -1,11 +1,16 @@
-//引数の値で constructor で初期化する場合
+// 引数の値で constructor で初期化するけど、引数がない場合も考慮する場合
 
 class A {
     public message: string;
-    constructor(m: string) {
-        this.message = m;
+    constructor(m?: string) {
+        if (m == undefined)
+            this.message = 'no value';
+        else
+            this.message = m;
     }
 }
 
 var a = new A('Hi');
-
+alert(a.message);
+var b = new A();
+alert(b.message);
